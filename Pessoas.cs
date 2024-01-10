e4c5406eaa3c74d7b3778e872414408f06d0f23f
Tony <antony.branquinho@gmail.com>
2024-01-10 10:52:41 -0300

    legal

---
+++ Pessoas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPatricaClasses {
+    internal class Pessoas {
+
+        public string nome;
+        public int idade;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using ExercicioPatricaClasses;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            Pessoas primeiraPessoa, segundaPessoa;
+
+            primeiraPessoa = new Pessoas();
+            segundaPessoa = new Pessoas();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            primeiraPessoa.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            primeiraPessoa.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.WriteLine("Nome: ");
+            segundaPessoa.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            segundaPessoa.idade = int.Parse(Console.ReadLine());
+
+            string maisVelha = "x";
+            if (primeiraPessoa.idade > segundaPessoa.idade) {
+                maisVelha = primeiraPessoa.nome;
+            }
+            else {
+                maisVelha = segundaPessoa.nome;
+            }
+
+            Console.WriteLine("Pessoa mais velha: " + maisVelha);
+        }
+    }
+}
